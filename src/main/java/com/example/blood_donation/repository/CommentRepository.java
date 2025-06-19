c466bc41d21fa435bede7bfa6c3890884e9ae7bc
ChiBao <baotochi87@gmail.com>
2025-06-19 16:03:53 +0700

    feat(comment): add two custom methods in CommentRepository

--- src/main/java/com/example/blood_donation/repository/CommentRepository.java
+++ src/main/java/com/example/blood_donation/repository/CommentRepository.java
@@ -2,8 +2,18 @@ package com.example.blood_donation.repository;
 
 import com.example.blood_donation.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.account WHERE c.blog.id = :blogId")
+    List<Comment> findCommentByBlogIdWithAccount(@Param("blogId") Long id);
+
+    @Query("SELECT c FROM Comment c WHERE c.blog.id = :blogId ORDER BY c.created_at DESC")
+    List<Comment> findRecentCommentsByBlogId(@Param("blogId") Long blogId);
 }
