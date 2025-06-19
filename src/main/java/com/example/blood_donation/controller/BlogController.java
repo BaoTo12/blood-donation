cf54ae269ef7cfaca673004f4deb7350ebf5776b
ChiBao <baotochi87@gmail.com>
2025-06-19 07:10:44 +0700

    feat(blog): add base controller, service, repo

--- src/main/java/com/example/blood_donation/controller/BlogController.java
+++ src/main/java/com/example/blood_donation/controller/BlogController.java
@@ -1,4 +1,16 @@
 package com.example.blood_donation.controller;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("${api.base-path}/blog")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BlogController {
+
 }
---
+++ src/main/java/com/example/blood_donation/service/BlogServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.blood_donation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogServiceImpl implements BlogService{
+}
