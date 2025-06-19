592f76148fc67fd214bce81cc07b6df480e0d61f
ChiBao <baotochi87@gmail.com>
2025-06-19 13:58:20 +0700

    feat(comment): add annotations in comment entity

--- src/main/java/com/example/blood_donation/entity/Comment.java
+++ src/main/java/com/example/blood_donation/entity/Comment.java
@@ -2,15 +2,23 @@ package com.example.blood_donation.entity;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Comment extends BaseEntity {
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "blog_id")
     private Blog blog;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
     private Account account;
 
     private String content;
