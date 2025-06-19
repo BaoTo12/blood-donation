994a3348eacc2879b5b875919ce96f96c40b5897
ChiBao <baotochi87@gmail.com>
2025-06-19 18:34:37 +0700

    fix(blog): add nullValuePropertyMappingStrategy for updateFromDto in BlogMapper

--- src/main/java/com/example/blood_donation/mapper/BlogMapper.java
+++ src/main/java/com/example/blood_donation/mapper/BlogMapper.java
@@ -7,9 +7,7 @@ import com.example.blood_donation.entity.Account;
 import com.example.blood_donation.entity.Blog;
 import com.example.blood_donation.exception.ResourceNotFoundException;
 import com.example.blood_donation.repository.AccountRepository;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
@@ -28,5 +26,6 @@ public abstract class BlogMapper {
 
     public abstract BlogResponse toBlogResponse(Blog blog);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     public abstract void updateFromDto(BlogUpdateRequest request, @MappingTarget Blog blog);
 }
