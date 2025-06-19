f23368dc55edef9407a34f4a44e95c5136f68653
ChiBao <baotochi87@gmail.com>
2025-06-19 14:13:20 +0700

    feat(comment): add validation for CommentRequest

--- src/main/java/com/example/blood_donation/dto/request/CommentRequest.java
+++ src/main/java/com/example/blood_donation/dto/request/CommentRequest.java
@@ -2,15 +2,19 @@ package com.example.blood_donation.dto.request;
 
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class CommentRequest {
+    @NotNull
     private Long blog_id;
 
+    @NotNull
     private Long account_id;
 
+    @NotNull
     private String content;
 }
