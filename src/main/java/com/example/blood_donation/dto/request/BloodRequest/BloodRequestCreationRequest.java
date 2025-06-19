d1e95d860afd2230c24c19c5aa797b000917b18a
ChiBao <baotochi87@gmail.com>
2025-06-19 18:31:04 +0700

    feat(bloodRequest): change validation of engTime from FutureOrPresent to Future

--- src/main/java/com/example/blood_donation/dto/request/BloodRequest/BloodRequestCreationRequest.java
+++ src/main/java/com/example/blood_donation/dto/request/BloodRequest/BloodRequestCreationRequest.java
@@ -4,6 +4,7 @@ import com.example.blood_donation.entity.Account;
 import com.example.blood_donation.enumType.BloodGroup;
 import com.example.blood_donation.enumType.RequestPriority;
 import com.example.blood_donation.validation.EnumValue;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -32,7 +33,7 @@ public class BloodRequestCreationRequest {
     private LocalDate startTime;
 
     @NotNull
-    @FutureOrPresent(message = "End time must be today or in the future")
+    @Future(message = "End time must be today or in the future")
     private LocalDate endTime;
 
     private Boolean is_active = Boolean.TRUE;
