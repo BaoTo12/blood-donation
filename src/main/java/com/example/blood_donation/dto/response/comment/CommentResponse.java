257a53a4d287f0457c0c16c359bca8e28ed41a38
ChiBao <baotochi87@gmail.com>
2025-06-19 16:03:14 +0700

    fix(comment):change from Blog, Account to BlogResponse, AccountResponse respectively in CommentResponse

--- src/main/java/com/example/blood_donation/dto/response/comment/CommentResponse.java
+++ src/main/java/com/example/blood_donation/dto/response/comment/CommentResponse.java
@@ -1,4 +1,22 @@
 package com.example.blood_donation.dto.response.comment;
 
+import com.example.blood_donation.dto.response.account.AccountResponse;
+import com.example.blood_donation.dto.response.blog.BlogResponse;
+import com.example.blood_donation.entity.Account;
+import com.example.blood_donation.entity.Blog;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
 public class CommentResponse {
+    private BlogResponse blog;
+
+    private AccountResponse account;
+
+    private String content;
+
+    private LocalDate created_at;
 }
