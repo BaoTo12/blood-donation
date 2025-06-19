dbdc0d4496d405d315c64091b726f26b21af394e
ChiBao <baotochi87@gmail.com>
2025-06-19 07:13:45 +0700

    feat(blog): add base class controller, service, repo

---
+++ src/main/java/com/example/blood_donation/service/BlogServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.blood_donation.service;
+
+import com.example.blood_donation.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BlogServiceImpl implements BlogService{
+    BlogRepository blogRepository;
+
+}
+
