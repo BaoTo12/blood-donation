2e5a74c0fd59df8dd85a0575b0f7c43dbae2214c
ChiBao <baotochi87@gmail.com>
2025-06-19 09:53:33 +0700

    feat(blog): add helper fetchAccount in blog mapper

--- src/main/java/com/example/blood_donation/mapper/BlogMapper.java
+++ src/main/java/com/example/blood_donation/mapper/BlogMapper.java
@@ -1,11 +1,25 @@
 package com.example.blood_donation.mapper;
 
 import com.example.blood_donation.dto.request.blog.BlogCreationRequest;
+import com.example.blood_donation.entity.Account;
 import com.example.blood_donation.entity.Blog;
+import com.example.blood_donation.exception.ResourceNotFoundException;
+import com.example.blood_donation.repository.AccountRepository;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface BlogMapper {
+public abstract class BlogMapper {
 
-    Blog toBlog(BlogCreationRequest request);
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Mapping(target = "account", expression = "java(fetchAccount(request.getAccount_id()))")
+    public abstract Blog toBlog(BlogCreationRequest request);
+
+    protected Account fetchAccount(Long id){
+        return accountRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Account not found: " + id));
+    }
 }
