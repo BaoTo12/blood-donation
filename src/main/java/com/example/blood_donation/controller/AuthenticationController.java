007cc7a841ed711fef28892c3423a3efe3e74709
ChiBao <baotochi87@gmail.com>
2025-06-22 07:37:38 +0700

    feat(auth): authorize by JWT

---
+++ src/main/java/com/example/blood_donation/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.example.blood_donation.controller;
+
+
+import com.example.blood_donation.dto.request.auth.AuthenticationRequest;
+import com.example.blood_donation.dto.request.auth.IntrospectRequest;
+import com.example.blood_donation.dto.response.ApiResponse;
+import com.example.blood_donation.dto.response.auth.AuthenticationResponse;
+import com.example.blood_donation.dto.response.auth.IntrospectResponse;
+import com.example.blood_donation.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("${api.base-path}/auth")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/token")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        var result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder()
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(authenticationService.introspect(request))
+                .build();
+    }
+}
