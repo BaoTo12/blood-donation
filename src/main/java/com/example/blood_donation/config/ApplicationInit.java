539005e1ea8f1b7ef27efd5cb82f5851538f3a70
ChiBao <baotochi87@gmail.com>
2025-06-23 15:34:04 +0700

    feat(appInit): init data admin

--- src/main/java/com/example/blood_donation/config/ApplicationInit.java
+++ src/main/java/com/example/blood_donation/config/ApplicationInit.java
@@ -1,7 +1,10 @@
 package com.example.blood_donation.config;
 
 import com.example.blood_donation.entity.Account;
+import com.example.blood_donation.entity.Role;
+import com.example.blood_donation.enumType.PreDefinedRole;
 import com.example.blood_donation.repository.AccountRepository;
+import com.example.blood_donation.repository.RoleRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -10,6 +13,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
@@ -17,12 +23,17 @@ public class ApplicationInit {
 
     PasswordEncoder passwordEncoder;
     AccountRepository accountRepository;
+    RoleRepository roleRepository;
 
     @Bean
     public ApplicationRunner applicationRunner() {
         return args -> {
             if (accountRepository.findByEmail("admin@gmail.com").isEmpty()) {
                 Account account = new Account();
+                Set<Role> roles = new HashSet<>();
+                Role role = roleRepository.findByName(PreDefinedRole.ADMIN.name());
+                roles.add(role);
+                account.setRoles(roles);
                 account.setPassword(passwordEncoder.encode("admin"));
                 account.setEmail("admin@gmail.com");
                 accountRepository.save(account);
