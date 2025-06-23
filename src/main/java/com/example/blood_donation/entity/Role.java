6212ed67310d6ea5072af34e4a9204b14f5faa9a
ChiBao <baotochi87@gmail.com>
2025-06-23 13:56:24 +0700

    feat(role): add Role Entity

--- src/main/java/com/example/blood_donation/entity/Role.java
+++ src/main/java/com/example/blood_donation/entity/Role.java
@@ -1,4 +1,18 @@
 package com.example.blood_donation.entity;
 
-public class Role {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Role extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String description;
+
 }
