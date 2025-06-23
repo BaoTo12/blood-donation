49857edba4f953a3fc4dd7e668d326719aea39bb
ChiBao <baotochi87@gmail.com>
2025-06-23 14:40:14 +0700

    fix(account): ditch singular role field in AccountResponse

--- src/main/java/com/example/blood_donation/dto/response/account/AccountResponse.java
+++ src/main/java/com/example/blood_donation/dto/response/account/AccountResponse.java
@@ -1,14 +1,16 @@
 package com.example.blood_donation.dto.response.account;
 
+import com.example.blood_donation.entity.Role;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Setter
 @Getter
 public class AccountResponse {
-    private String role;
 
     private String email;
 
@@ -25,4 +27,6 @@ public class AccountResponse {
     private LocalDate birthday;
 
     private boolean active = true;
+
+    private Set<Role> roles = new HashSet<>();
 }
