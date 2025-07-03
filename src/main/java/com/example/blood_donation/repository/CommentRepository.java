95cb71606c429ed77a692609470cf311ed05999c
ChiBao <baotochi87@gmail.com>
2025-07-03 15:00:56 +0700

    chore: add comment in comment repo class

--- src/main/java/com/example/blood_donation/repository/CommentRepository.java
+++ src/main/java/com/example/blood_donation/repository/CommentRepository.java
@@ -11,13 +11,14 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    // This generates a SELECT with shared locks for reading
     @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.account WHERE c.blog.id = :blogId")
     List<Comment> findCommentByBlogIdWithAccount(@Param("blogId") Long id);
 
     @Query("SELECT c FROM Comment c WHERE c.blog.id = :blogId ORDER BY c.created_at DESC")
     List<Comment> findRecentCommentsByBlogId(@Param("blogId") Long blogId);
 
+    // This method, when called within a transaction, will acquire appropriate locks
     @Modifying
     @Query("UPDATE Comment c SET c.content = :content WHERE c.id = :id ")
     Integer updateComment(@Param("content") String content, @Param("id") Long id);
